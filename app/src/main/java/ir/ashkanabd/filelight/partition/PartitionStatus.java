095ad62fd7334505bc87bde02136e04aa187a9c6
AshkanAbd <kotksthegame@gmail.com>
2019-04-29 23:14:09 +0430

    Change partition free and total space to long. Add path to partition status.

--- app/src/main/java/ir/ashkanabd/filelight/partition/PartitionStatus.java
+++ app/src/main/java/ir/ashkanabd/filelight/partition/PartitionStatus.java
@@ -4,33 +4,38 @@ import androidx.annotation.NonNull;
 
 public class PartitionStatus {
 
-    private int totalSpace;
-    private int freeSpace;
+    private long totalSpace;
+    private long freeSpace;
     private String partitionName;
+    private String path;
 
-    public PartitionStatus(int totalSpace, int freeSpace, String partitionName) {
+    public PartitionStatus(long totalSpace, long freeSpace, String partitionName) {
         this.totalSpace = totalSpace;
         this.freeSpace = freeSpace;
         this.partitionName = partitionName;
     }
 
-    public double getPercent() {
-        return 100 / (totalSpace / (double) freeSpace);
+    public PartitionStatus() {
     }
 
-    public int getTotalSpace() {
+    public double getPercent() {
+        long nonfree = totalSpace - freeSpace;
+        return 100 / (totalSpace / (double) nonfree);
+    }
+
+    public long getTotalSpace() {
         return totalSpace;
     }
 
-    public void setTotalSpace(int totalSpace) {
+    public void setTotalSpace(long totalSpace) {
         this.totalSpace = totalSpace;
     }
 
-    public int getFreeSpace() {
+    public long getFreeSpace() {
         return freeSpace;
     }
 
-    public void setFreeSpace(int freeSpace) {
+    public void setFreeSpace(long freeSpace) {
         this.freeSpace = freeSpace;
     }
 
@@ -49,6 +54,35 @@ public class PartitionStatus {
                 "totalSpace=" + totalSpace +
                 ", freeSpace=" + freeSpace +
                 ", partitionName='" + partitionName + '\'' +
+                ", path='" + path + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PartitionStatus that = (PartitionStatus) o;
+
+        if (totalSpace != that.totalSpace) return false;
+        if (freeSpace != that.freeSpace) return false;
+        return partitionName.equals(that.partitionName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (totalSpace ^ (totalSpace >>> 32));
+        result = 31 * result + (int) (freeSpace ^ (freeSpace >>> 32));
+        result = 31 * result + partitionName.hashCode();
+        return result;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 }
